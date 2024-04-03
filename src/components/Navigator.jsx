aac0df307c693a38319cf87f118558ef3450995b
Rojas259 <147790485+Rojas259@users.noreply.github.com>
2024-04-02 21:39:26 -0700

    nav update function

--- src/components/Navigator.jsx
+++ src/components/Navigator.jsx
@@ -2,15 +2,16 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import { Link } from 'react-router-dom';
 
-import Container from "@mui/material/Container";
-import Menu from "@mui/material-icons/Menu";
-import IconButton from "@mui/material/IconButton";
-import MenuItem from "@mui/material/MenuItem";
-import { useTheme } from "@mui/material/styles";
+import MenuItem from '@mui/material/MenuItem';
+import { useTheme } from '@mui/material';
 
 const pages = [ 'About', 'Protfolio', 'Contact', 'Resume' ];
 const settings = [ 'Profile', 'Account', 'Dashboard', 'Logout' ];
