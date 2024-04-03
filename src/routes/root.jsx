51a9ef921e7fc8e7dd083263bb2442d81db21797
Rojas259 <147790485+Rojas259@users.noreply.github.com>
2024-04-02 18:06:34 -0700

    fixed routes for root

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,17 +1,19 @@
 import { Outlet } from "react-router-dom";
 import { Container, Box } from "@mui/themed";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import Header from "../components/Header.jsx";
+import Footer from "../components/Footer.jsx";
 
 
 export default function Root() {
     return (
-        <Container>
+        <>
+        <Container maxWidth = "md">
             <Header />
-            <Box>
+            <Box sx = {{p:2}}>
                 <Outlet />
             </Box>
             <Footer />
         </Container>
-    );
+        </>
+    )
 }
