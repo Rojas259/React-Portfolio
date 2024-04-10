28ff03e46b388c7a792da2a1e7a94511371f0c4b
Rojas259 <147790485+Rojas259@users.noreply.github.com>
2024-04-09 20:23:31 -0700

    missing

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -37,15 +37,6 @@ export default function AboutMe() {
                                 play basketball or football. I would also go to the gym to work out and stay in shape. I would also go to the beach to relax    
                                 
                             </Typography><br></br>
-                            <Typography>
-                                Before working at this job I was working on my Associates in Science and continue working on my Biotechnology. 
-                                I was working for 40 to 60 hours a week for work and have class in the afternoons. So, I have experience in working long
-                                hours and maintain class work at the sametime. I have also have done community service and help in my religion for programs
-                                with Junior Youth and youth programs and projects in my free time. I would read mangas and books for genes of science 
-                                fiction, Fantasy, and mostly afterlife/purgatory stories. I also would draw things that would pop up in my head at the time.
-                                I would play Video games in my free time to relax after a long week of work and school, if was not going out my friends to
-                                play basketball or football.                                
-                            </Typography>
                         </Box>
                     </Paper>
                 </Grid>
